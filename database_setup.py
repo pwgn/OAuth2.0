4b8146e6312fea26c36ab75b781facca209d72db
Christoffer Persson <christoffer@fripost.org>
2017-03-03 09:12:01 +0100

    user: add user db entity

--- database_setup.py
+++ database_setup.py
@@ -2,14 +2,34 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250), nullable=True)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'id'           : self.id,
+           'name'         : self.name,
+           'email'        : self.email,
+           'picture'      : self.picture,
+       }
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -17,8 +37,9 @@ class Restaurant(Base):
        return {
            'name'         : self.name,
            'id'           : self.id,
+           'user_id'      : self.user_id
        }
- 
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
@@ -30,7 +51,8 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
-
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -41,11 +63,12 @@ class MenuItem(Base):
            'id'         : self.id,
            'price'         : self.price,
            'course'         : self.course,
+           'user_id'      : self.user_id
        }
 
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
- 
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+
 
 Base.metadata.create_all(engine)
